c6179287145fc4f2a1a2c2fd09c8eef86b54ca43
AhmeWagih <ahmedwagih087@gmail.com>
2024-12-01 18:22:12 +0200

    init

---
+++ name/src/assets/images/index.js
@@ -0,0 +1,36 @@
+import bigShoe1 from "./download.png";
+import bigShoe2 from "./download1.jpg";
+import bigShoe3 from "./download2.jpg";
+import bigShoe4 from "./download.png";
+
+import Logo from './logo.jpeg'
+import avatar from './avatar.png';
+
+import shoe4 from "./download.png";
+import shoe5 from "./download1.jpg";
+import shoe6 from "./download2.jpg";
+import shoe7 from "./download3.jpg";
+import shoe8 from "./download.png";
+
+import thumbnailBackground from "./thumbnail-background.svg";
+import thumbnailShoe1 from "./download.png";
+import thumbnailShoe2 from "./download1.jpg";
+import thumbnailShoe3 from "./download2.jpg";
+
+export {
+  Logo,
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  bigShoe4,
+  avatar,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  shoe8,
+  thumbnailBackground,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+};
